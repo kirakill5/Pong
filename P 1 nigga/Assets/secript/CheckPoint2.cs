deb8cac2a5f08b8f313de687deae3ba2db68e287
kirakill5 <65878242+kirakill5@users.noreply.github.com>
2020-10-24 18:05:44 +0200

    CP2
    no thing

---
+++ P 1 nigga/Assets/secript/CheckPoint2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint2 : MonoBehaviour
+{
+
+    public int a;
+    public int b;
+
+    public void Start  ()
+    {
+       Debug.Log(Sum(a, b));
+       Debug.Log(Prod(a, b)); 
+       Debug.Log(div(a, b));
+    }
+
+    float Sum(float a, float b)
+    {
+        return a + b;
+    }
+    float Prod(float a, float b)
+    {
+        return a * b;
+    }
+    float div(float a, float b)
+    {
+        if (b != 0)
+        {
+            return a /b;
+        }
+        else
+        {
+            Debug.Log("an error");
+            return 0;
+        }
+    }
+
+}
+
+  
